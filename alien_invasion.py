77bf04412a8558a2ae741fa21aedd8de248f7ce2
Ilya <55929539+Markalfox@users.noreply.github.com>
2022-10-10 20:47:38 +0300

    Refactoring. Added methods __check_keydown_events and __check_keyup_events

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,15 +28,21 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self.__check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self.__check_keyup_events(event)
+
+    def __check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def __check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def __update_screen(self):
         self.screen.fill(self.settings.bg_color)
